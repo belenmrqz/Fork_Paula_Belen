35e02d0eaedbee4807e7ae701f3a26601c8f895b
Paula Sanchez Velez <psanchezvelez03@gmail.com>
2025-12-01 20:30:03 +0100

    Esbozo flujo del main

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+from config.constantes import IPC,  IPV, TASA_PARO, TEMPORALIDAD, EAES_OCUPACION, EAES_PERCENTILES, ETCL
+from src.inedata import INEDataExtractor
+from src.procesar import procesar_datos
+from src.almacenar import insertar_datos
+from src.db import DatabaseConnection, crear_base_datos
+
+
 def main():
-    print()
+   
+   db = DatabaseConnection.get_connection()
+   crear_base_datos()
+
+   tablas = [IPC, IPV, TASA_PARO, TEMPORALIDAD, EAES_OCUPACION, EAES_PERCENTILES, ETCL]
+   for codigo in tablas:
+        extractor = INEDataExtractor(codigo)
+        if extractor.obtener_datos():
+           datos_procesados = procesar_datos(db, codigo, extractor.raw_data)
+           insertar_datos(db, datos_procesados)
+        else:
+            print(f"No se pudieron obtener los datos de la tabla {codigo}")
+
+
+   DatabaseConnection.close()
 
 if __name__ == "__main__":
     main()
